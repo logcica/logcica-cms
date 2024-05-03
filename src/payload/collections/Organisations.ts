acfa22a7162d33d8241caafffb584b925ca3a692
Olivier Wouters <olivier5741@gmail.com>
2024-05-03 16:22:35 +0200

    BCE cell link

--- src/payload/collections/Organisations.ts
+++ src/payload/collections/Organisations.ts
@@ -1,54 +1,64 @@
 import type { Access, CollectionConfig } from 'payload/types'
 import CustomImageCell from '../fields/CustomImageCell'
-import { canManageOrContribute } from './canRead';
-import ownerPartyField from '../fields/ownerPartyField';
-import partyField from '../fields/partyField';
+import { canManageOrContribute } from './canRead'
+import ownerPartyField from '../fields/ownerPartyField'
+import partyField from '../fields/partyField'
+import BCELinkCell from '../fields/BCELinkCell'
 
 const Organisations: CollectionConfig = {
   slug: 'organisations',
   admin: {
     useAsTitle: 'name',
-    defaultColumns: ["name", "place"],
+    defaultColumns: ['name', 'place'],
     group: 'Structure',
-    listSearchableFields: ['name','place.name']
+    listSearchableFields: ['name', 'number', 'place.name'],
   },
   access: {
-    read: canManageOrContribute({placeInProperty: 'place'}),
+    read: canManageOrContribute({ placeInProperty: 'place' }),
   },
   fields: [
     {
       type: 'row',
       fields: [
-      {
-        name: 'number',
-        type: 'text',
-      },
-      {
-        name: 'name',
-        type: 'text',
-      },
-      {
-        name: 'legalForm',
-        type: 'relationship',
-        relationTo: 'codes',
-        /* -> is not sorting alpha. afterwards ...
+        {
+          name: 'number',
+          type: 'text',
+          admin: {
+            components: {
+              Cell: BCELinkCell,
+            },
+          },
+        },
+        {
+          name: 'name',
+          type: 'text',
+        },
+        {
+          name: 'legalFormShort',
+          type: 'text',
+        },
+        {
+          name: 'legalForm',
+          type: 'relationship',
+          relationTo: 'codes',
+          /* -> is not sorting alpha. afterwards ...
         admin: {
           sortOptions: 'rank'
         },*/
-        filterOptions: () => {
-          return {
-            list: { equals: '6613b53f2c29cc450c474e3f' },
-            skip: { not_equals: true },
-          }
+          filterOptions: () => {
+            return {
+              list: { equals: '6613b53f2c29cc450c474e3f' },
+              skip: { not_equals: true },
+            }
+          },
         },
-      },
-      ]
+      ],
     },
     {
       name: 'registeredAt',
-      type: 'date'
+      type: 'date',
     },
-    partyField({name: 'owner', relations: ['partnerships', 'persons']}),
+    partyField({ name: 'owner', relations: ['partnerships', 'persons'] }),
     {
       name: 'place',
       type: 'relationship',
@@ -57,32 +67,32 @@ const Organisations: CollectionConfig = {
     },
     {
       name: 'mainImage',
-      label: "Image",
+      label: 'Image',
       type: 'group',
-      interfaceName: "Image",
+      interfaceName: 'Image',
       admin: {
         components: {
-          Cell: CustomImageCell
-        }
+          Cell: CustomImageCell,
+        },
       },
       fields: [
         {
-          name:'url',
-          type: 'text'
-        }
-      ]
+          name: 'url',
+          type: 'text',
+        },
+      ],
     },
     {
       name: 'images',
       type: 'array',
-      interfaceName: "Images",
+      interfaceName: 'Images',
       fields: [
         {
-          name:'url',
-          type: 'text'
-        }
-      ]
-    }
+          name: 'url',
+          type: 'text',
+        },
+      ],
+    },
   ],
 }
 
