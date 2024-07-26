006f9a8bcf87d51a307b64e1beccda78a24be0b5
BenjaminGuillaumat <120639167+BenjaminGuillaumat@users.noreply.github.com>
2024-07-26 11:26:08 +0200

    categories title

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -2,12 +2,13 @@ import type { CollectionConfig } from 'payload/types'
 import { anyone } from '../access/anyone'
 import { cannotConfigure } from './canRead'
 import {getCollectionLabelsTranslations, getLabelTranslations} from "../utilities/translate";
+import { Classification, InformationSystem } from 'payload/generated-types';
 
 const Categories: CollectionConfig = {
   slug: 'categories',
   labels: getCollectionLabelsTranslations('categories'),
   admin: {
-    useAsTitle: 'name',
+    useAsTitle: 'title',
     group: 'Configuration',
     hidden: cannotConfigure
   },
@@ -44,7 +45,33 @@ const Categories: CollectionConfig = {
       label: getLabelTranslations('classification'),
       relationTo: 'classifications',
       hasMany: false,
-    }
+    },
+    {
+      name: 'title',
+      type: 'text',
+      admin: {
+        disableBulkEdit: true
+      },
+      hooks: {
+        afterRead: [
+          async ({ data, req }) => {  
+
+            if(!data.classification)
+              return data.name
+
+            const classification: Classification = await req.payload.findByID({
+              collection: 'classifications',
+              id: data.classification,
+            })
+
+            if(!classification?.system)
+              return data.name
+
+            return data.name + " (" + (classification.system as InformationSystem).name + ")"
+          }
+        ],
+      },
+    },
   ],
 }
 
