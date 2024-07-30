4762125c1912bd4046df57422b288c8ac6d3f95c
Olivier Wouters <olivier5741@gmail.com>
2024-07-30 12:18:35 +0200

    classification subject

--- src/collections/Classifications.ts
+++ src/collections/Classifications.ts
@@ -1,7 +1,7 @@
 import type { CollectionConfig } from 'payload/types'
 import { cannotConfigure } from './canRead'
-import nameField from "../fields/nameField";
-import {getCollectionLabelsTranslations, getLabelTranslations} from "../utilities/translate";
+import nameField from '../fields/nameField'
+import { getCollectionLabelsTranslations, getLabelTranslations } from '../utilities/translate'
 
 const Classifications: CollectionConfig = {
   slug: 'classifications',
@@ -9,25 +9,40 @@ const Classifications: CollectionConfig = {
   admin: {
     useAsTitle: 'name',
     group: 'Configuration',
-    hidden: cannotConfigure
+    hidden: cannotConfigure,
   },
   access: {
     read: () => true,
   },
   fields: [
     {
-      name: 'key',
-      type: 'text',
-      label: getLabelTranslations('key'),
+      type: 'row',
+      fields: [
+        {
+          name: 'key',
+          type: 'text',
+          label: getLabelTranslations('key'),
+        },
+        nameField,
+      ],
     },
-    nameField,
     {
-      name: 'system',
-      type: 'relationship',
-      label: getLabelTranslations('system'),
-      relationTo: 'information_systems',
-      hasMany: false,
-    }
+      type: 'row',
+      fields: [
+        {
+          name: 'system',
+          type: 'relationship',
+          label: getLabelTranslations('system'),
+          relationTo: 'information_systems',
+          hasMany: false,
+        },
+        {
+          name: 'subject',
+          type: 'text',
+          label: getLabelTranslations('subject'),
+        },
+      ],
+    },
   ],
 }
 
